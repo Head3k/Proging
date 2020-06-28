bc445c6d9a2c04fdf3bd07e795be66b3797c24ce
He3k_Kubuntu <Nickolablaniy@mail.ru>
2020-06-29 02:11:27 +0700

    Update Lab2

--- Lab2/src/main.c
+++ Lab2/src/main.c
@@ -3,60 +3,102 @@
 
 int main()
 {
-    IntVector *a, *b;
-    a = int_vector_new(7);//
-    printf("\n-- new_vector --");
-    for (int i = 0; i < 6; i++)
-    {
-        a->size++;
-        a->arr[i] = i;
-    }
+    IntVector *a, *b;//
+    a = int_vector_new(7);// void create array
+    printf("\n-- new_vector --"); //
+    for (int i = 0; i < 6; i++){ // completion array
+        a->arr[i] = i; // completion element
+	    a->size++; // count size
+    }//
     printf("\nA = ");//
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 6; i++){ // print array 
+        printf("%d ", a->arr[i]); // print element
+    }//
+    //
+    //
+    b = int_vector_copy(a); // void copy array
+    printf("\n\n-- copy --");//
+    printf("\nB = "); //
+    for (int i = 0; i < 6; i++) // print second array
     {
-        printf("%d ", a->arr[i]);
-    }
-    b = int_vector_copy(a);//
-    printf("\n\n-- copy --");
-    printf("\nB = ");//
-    for (int i = 0; i < 6; i++)
-    {
-        printf("%d ", b->arr[i]);
+        printf("%d ", b->arr[i]); // print element
     }
     printf("\n\n-- push_back ==> 7 --");
-    int_vector_push_back(b, 7);//
-    printf("\narr[6] ==> %d \ncapacity ==> %zd \nsize ==> %zd\n", b->arr[6], 
-
-int_vector_get_capacity(b), int_vector_get_size(b));//
+    //
+    //
+    int_vector_push_back(b, 7); // void add to last element
+    //
+    printf("\narray ==> %ld", int_vector_get_size(b));//
+    printf("\ncapacity ==> %zd", int_vector_get_capacity(b));//
+    printf("\nsize ==> %zd",int_vector_get_size(b));//
+    //
     printf("\nB = ");//
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < 8; i++)
     {
         printf("%d ", b->arr[i]);//
     }
-    int_vector_push_back(b, 7);
-    printf("\narr[6] ==> %d \ncapacity ==> %zd \nsize ==> %zd\n", b->arr[6], 
-
-int_vector_get_capacity(b), int_vector_get_size(b));//
+    //
+    //
+    printf("\n\n-- NEW push_back ==> 7 --");
+    int_vector_push_back(b, 7); // void add to
+    //
+    printf("\narray ==> %ld", int_vector_get_size(b));//
+    printf("\ncapacity ==> %zd", int_vector_get_capacity(b));//
+    printf("\nsize ==> %zd",int_vector_get_size(b));//
+    //
     printf("\nB = ");//
-    int_vector_set_item(b, 0, 9);
-    printf("get item ==> %d\n", int_vector_get_item(b, 0));//
-    int_vector_pop_back(b);
-    printf("\n-- pop_back --");
-    printf("\ncapacity ==> %zd\nsize ==> %zd\n", int_vector_get_capacity(b), 
-
-int_vector_get_size(b));//
-    int_vector_shrink_to_fit(b);
-    printf("\n-- shrink_to_fit --");
-    printf("\ncapacity ==> %zd\n", int_vector_get_capacity(b));//
-    int_vector_resize(b, 5);
-    printf("get_size ==> %zd\n", int_vector_get_size(b));//
-    int_vector_reserve(b, 5);
-    printf("\n-- reserve ==> 5 --");
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%d ", b->arr[i]);//
+    }
+    //
+    //
+    printf("\n\n-- set_item i[0] ==> 9 --");//
+    int_vector_set_item(b, 0, 9); // void 9 --> i[0] from the array b
+    //
+    //
+    printf("\n\n-- get_item i[0] -- \n"); // 
+    printf("item[0] = %d", int_vector_get_item(b, 0)); // void get item i[0] from the array b
+    for (int i = 0; i < 8; i++)
     {
         printf("\n arr:: %d ", b->arr[i]);//
     }
-    printf("\ncapacity ==> %zd\n", int_vector_get_capacity(b));//
+    //
+    //
+    printf("\n\n-- pop_back --"); //
+    int_vector_pop_back(b); // void delete last element array
+    //
+    for (int i = 0; i < 8; i++)
+    {
+        printf("\n arr:: %d ", b->arr[i]);//
+    }
+    //
+    //
+    printf("\ncapacity ==> %zd", int_vector_get_capacity(b));//
+    printf("\nsize ==> %zd",int_vector_get_size(b));//
+    //
+    //
+    printf("\n\n-- shrink_to_fit --"); // void 
+    int_vector_shrink_to_fit(b);//
+    //
+    printf("\ncapacity ==> %zd", int_vector_get_capacity(b));//
+    printf("\nsize ==> %zd",int_vector_get_size(b));//
+    //
+    //
+    printf("\n\n-- resize ==> 10 --"); //
+    int_vector_resize(b, 10); //
+    //
+    printf("\ncapacity ==> %zd", int_vector_get_capacity(b));//
+    printf("\nsize ==> %zd",int_vector_get_size(b));//
+    //
+    //
+    printf("\n\n-- reserve ==> 15 --");
+    int_vector_reserve(b, 15);
+    //
+    printf("\ncapacity ==> %zd", int_vector_get_capacity(b));//
+    printf("\nsize ==> %zd\n",int_vector_get_size(b));//
+    //
+    //
     int_vector_free(b);
     int_vector_free(a);
     return 0;
